3465cf9e5e2f1cdebae3c83e5ee184cc1ffc7c7a
Prachi Doshi <prachidoshi@users.noreply.github.com>
2017-09-25 16:59:14 -0700

    Update CountPrimes.java

--- Hashing/CountPrimes.java
+++ Hashing/CountPrimes.java
@@ -1,4 +1,3 @@
-Attempt2: https://leetcode.com/problems/count-primes/ explains it well
 1. Ignore 1 and n. Don't count 1 and the number itself in.
 2. Assume all numbers are prime in a boolean[]. Check off those are certainly not prime, the remaining will be prime.
 3. For any n, only need to check up to i * i < n; more than that, for example 2 x 6 is same as checking 6x2, but 6x2 is not necessary to check.
@@ -7,7 +6,7 @@ Attempt2: https://leetcode.com/problems/count-primes/ explains it well
 	Then more non-primes:self * self, self * (self + 1), self * (self + 2) ... etc.
 	So, mark all of these index of in the boolean[]
 	
-*/
+
 public class Solution {
     public int countPrimes(int n) {
     	if (n <= 1) {
