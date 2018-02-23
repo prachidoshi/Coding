062a49e3b3fc0b54a1a3f23884d7b69fe6369966
Prachi Doshi <prachidoshi@users.noreply.github.com>
2018-02-23 13:54:51 -0800

    Create  TwoStringsAreAnagrams.java

---
+++ LintCode/TwoStringsAreAnagrams.java
@@ -0,0 +1,10 @@
+/*
+Write a method anagram(s,t) to decide if two strings are anagrams or not.
+Example
+Given s = "abcd", t = "dcab", return true.
+Given s = "ab", t = "ab", return true.
+Given s = "ab", t = "ac", return false.
+
+Challenge 
+O(n) time, O(1) extra space
+*/
